c2c61b621ff3465c241d111b7d4d3439dda76d3a
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-03-30 03:21:27 +0200

    feat(kk-71): implement the route for each controller
    Changes made collaboratively making use of Live Share. Session
    participants: Juan, Rosalía.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,20 @@
 <?php
 
-use App\Http\Controllers\MinionController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArmorController;
+use App\Http\Controllers\MinionController;
+use App\Http\Controllers\WeaponController;
+use App\Http\Controllers\PlayerController;  
+use App\Http\Controllers\RareEventController;
+use App\Http\Controllers\SpecialEventController;
+use App\Http\Controllers\MainCharacterController;
+use App\Http\Controllers\JosephChaoticEventController;
 
+Route::apiResource("armor", ArmorController::class);
+Route::apiResource("players", PlayerController::class);
 Route::apiResource("minions", MinionController::class);
+Route::apiResource("weapons", WeaponController::class);
+Route::apiResource("rare-events", RareEventController::class);
+Route::apiResource("special-events", SpecialEventController::class);
+Route::apiResource("main-characters", MainCharacterController::class);
+Route::apiResource("chaotic-events", JosephChaoticEventController::class);
