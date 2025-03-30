ed66d995b037de16e31eb9aa9afc5fee4116ed47
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-03-30 03:36:04 +0200

    feat(kk-71): create the model and controller for "players" db table
    Changes made collaboratively making use of Live Share. Session
    participants: Juan, Rosalía.

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Player;
+use Illuminate\Http\Request;
+
+class PlayerController extends Controller
+{
+    public function index()
+    {
+        return Player::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:50',
+            'email_address' => 'required|string|email|max:254|unique:players,email_address',
+            'password' => 'required|string|min:6',
+        ]);
+        return Player::create($request->all());
+    }
+
+    public function show(string $id)
+    {
+        return Player::findOrFail($id);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $player = Player::findOrFail($id);
+        $player->update($request->all());
+
+        return $player;
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
