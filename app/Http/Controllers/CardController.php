cd7b16bbd0bf8e0a8fdb08c1e6b1dc4e0cf1c837
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-04-03 00:05:08 +0200

    feat: add the joseph events to the main character json response

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -15,7 +15,9 @@ class CardController extends Controller
     public function index()
     {
         return response()->json([
-            'main_characters' => MainCharacter::orderBy('main_character_id', 'asc')->get(),
+          'main_characters' => MainCharacter::with('josephChaoticEvents')
+            ->orderBy('main_character_id', 'asc')
+            ->get(),
             'minions' => Minion::orderBy('minion_id', 'asc')->get(),
             'weapons' => Weapon::orderBy('id', 'asc')->get(),
             'armor' => Armor::orderBy('id', 'asc')->get(),
