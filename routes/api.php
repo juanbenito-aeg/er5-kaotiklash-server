ded0e5602b0fbd952c8bd10f075e80e0254cd4de
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-04-02 22:37:59 +0200

    refactor(kk-84): create a Card Controller to get all types of cards bd tables in a one request

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,4 @@
 <?php
+use App\Http\Controllers\CardController;
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ArmorController;
-use App\Http\Controllers\MinionController;
-use App\Http\Controllers\WeaponController;
-use App\Http\Controllers\PlayerController;  
-use App\Http\Controllers\RareEventController;
-use App\Http\Controllers\SpecialEventController;
-use App\Http\Controllers\MainCharacterController;
-use App\Http\Controllers\JosephChaoticEventController;
-
-Route::apiResource("armor", ArmorController::class);
-Route::apiResource("players", PlayerController::class);
-Route::apiResource("minions", MinionController::class);
-Route::apiResource("weapons", WeaponController::class);
-Route::apiResource("rare-events", RareEventController::class);
-Route::apiResource("special-events", SpecialEventController::class);
-Route::apiResource("main-characters", MainCharacterController::class);
-Route::apiResource("joseph-chaotic-events", JosephChaoticEventController::class);
+Route::apiResource("cards", CardController::class);
