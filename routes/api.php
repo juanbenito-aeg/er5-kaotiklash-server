910b480aeec92e1ad00472b8ea2310875d34d41c
Rosalia Campusano <rosalia.campusano@ikasle.aeg.eus>
2025-04-02 23:22:54 +0200

    fix: add the player route

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CardController;
+use App\Http\Controllers\PlayerController;  
 
 Route::apiResource("cards", CardController::class);
+Route::apiResource("players", PlayerController::class);
