de2b64695a54221d15f618ee3b030ddfcaabb187
Eneko Azkue <eneko.azkue@ikasle.aeg.eus>
2025-05-06 12:06:14 +0200

    feat(kk-199): tweak getters to return adequate players stats data sets

--- app/Http/Controllers/PlayerStatsController.php
+++ app/Http/Controllers/PlayerStatsController.php
@@ -50,7 +50,6 @@ class PlayerStatsController extends Controller
     
     public function getTotalPlayedRounds($id)
     {
-
         $playedRoundsAsP1 = PlayerStats::where('player_1', $id)->sum('played_rounds');
         $playedRoundsAsP2 = PlayerStats::where('player_2', $id)->sum('played_rounds');
         $totalPlayedRounds = $playedRoundsAsP1 + $playedRoundsAsP2;
@@ -59,7 +58,27 @@ class PlayerStatsController extends Controller
             return response()->json(['message' => 'No played rounds found']);
         }
 
-        return response()->json($totalPlayedRounds, 200);
+        $playedRounds = [];
+
+        $gameIds = PlayerStats::where('player_1', $id)->orWhere('player_2', $id)->get(['id'])->toArray();
+
+        for ($i = 0; $i < count($gameIds); $i++) {
+            $playedRoundsAsP1InGame = PlayerStats::where('id', $gameIds[$i])->where('player_1', $id)->sum('played_rounds');
+            $playedRoundsAsP2InGame = PlayerStats::where('id', $gameIds[$i])->where('player_2', $id)->sum('played_rounds');
+
+            array_push($playedRounds, $playedRoundsAsP1InGame + $playedRoundsAsP2InGame);
+        }
+
+        $averagePlayedRounds = $totalPlayedRounds / count($gameIds);
+        $averagePlayedRounds = round($averagePlayedRounds, 2);
+
+        $playedRoundsData = [
+            'total_played_rounds' => $totalPlayedRounds,
+            'played_rounds' => $playedRounds,
+            'average_played_rounds' => $averagePlayedRounds,
+        ];
+
+        return response()->json($playedRoundsData, 200);
     }
 
     public function getJosephAppeared($id)
@@ -73,7 +92,24 @@ class PlayerStatsController extends Controller
             return response()->json(['message' => 'Joseph did not appear in any game']);
         }
 
-        return response()->json($totalJosephAppeared, 200);
+        $josephNotAppearedInP1 = PlayerStats::where('player_1', $id)->where('joseph_appeared', false)->count();
+        $josephNotAppearedInP2 = PlayerStats::where('player_2', $id)->where('joseph_appeared', false)->count();
+        $totalJosephNotAppeared = $josephNotAppearedInP1 + $josephNotAppearedInP2;
+        
+        $totalJoseph = $totalJosephAppeared + $totalJosephNotAppeared;
+        $percentageJosephAppeared = ($totalJosephAppeared / $totalJoseph) * 100;
+        $percentageJosephNotAppeared = ($totalJosephNotAppeared / $totalJoseph) * 100;
+        $percentageJosephAppeared = round($percentageJosephAppeared, 2);
+        $percentageJosephNotAppeared = round($percentageJosephNotAppeared, 2);
+
+        $josephAppearedData = [
+            'total_joseph_appeared' => $totalJosephAppeared,
+            'total_joseph_not_appeared' => $totalJosephNotAppeared,
+            'percentage_joseph_appeared' => $percentageJosephAppeared,
+            'percentage_joseph_not_appeared' => $percentageJosephNotAppeared,
+        ];
+
+        return response()->json($josephAppearedData, 200);
     }
 
     public function getTotalMinionsKilled($id)
@@ -87,7 +123,26 @@ class PlayerStatsController extends Controller
             return response()->json(['message' => 'No minions killed found']);
         }
 
-        return response()->json($totalMinionsKilled, 200);
+        $minionsKilled = [];
+
+        $gameIds = PlayerStats::where('player_1', $id)->orWhere('player_2', $id)->get(['id'])->toArray();
+
+        for ($i = 0; $i < count($gameIds); $i++) {
+            $minionsKilledAsP1InGame = PlayerStats::where('id', $gameIds[$i])->where('player_1', $id)->sum('player_1_minions_killed');
+            $minionsKilledAsP2InGame = PlayerStats::where('id', $gameIds[$i])->where('player_2', $id)->sum('player_2_minions_killed');
+
+            array_push($minionsKilled, $minionsKilledAsP1InGame + $minionsKilledAsP2InGame);
+        }
+
+        $averageMinionsKilled = $totalMinionsKilled / count($gameIds);
+        $averageMinionsKilled = round($averageMinionsKilled, 2);
+
+        $minionsKilledData = [
+            'total_minions_killed' => $totalMinionsKilled,
+            'minions_killed' => $minionsKilled,
+            'average_minions_killed' => $averageMinionsKilled,
+        ];
+        return response()->json($minionsKilledData, 200);
     }
 
     public function getTotalFumbles($id)
@@ -100,7 +155,28 @@ class PlayerStatsController extends Controller
         if ($totalFumbles == 0) {
             return response()->json(['message' => 'No fumbles found']);
         }
-        return response()->json($totalFumbles, 200);
+
+        $fumbles = [];
+
+        $gameIds = PlayerStats::where('player_1', $id)->orWhere('player_2', $id)->get(['id'])->toArray();
+
+        for ($i = 0; $i < count($gameIds); $i++) {
+            $fumblesAsP1InGame = PlayerStats::where('id', $gameIds[$i])->where('player_1', $id)->sum('player_1_fumbles');
+            $fumblesAsP2InGame = PlayerStats::where('id', $gameIds[$i])->where('player_2', $id)->sum('player_2_fumbles');
+
+            array_push($fumbles, $fumblesAsP1InGame + $fumblesAsP2InGame);
+        }
+
+        $averageFumbles = $totalFumbles / count($gameIds);
+        $averageFumbles = round($averageFumbles, 2);
+
+        $fumblesData = [
+            'total_fumbles' => $totalFumbles,
+            'fumbles' => $fumbles,
+            'average_fumbles' => $averageFumbles,
+        ];
+
+        return response()->json($fumblesData, 200);
     }
 
     public function getTotalCriticalHits($id)
@@ -113,7 +189,28 @@ class PlayerStatsController extends Controller
         if ($totalCrits == 0) {
             return response()->json(['message' => 'No critical hits found']);
         }
-        return response()->json($totalCrits, 200);
+    
+        $crits = [];
+
+        $gameIds = PlayerStats::where('player_1', $id)->orWhere('player_2', $id)->get(['id'])->toArray();
+
+        for ($i = 0; $i < count($gameIds); $i++) {
+            $critsAsP1InGame = PlayerStats::where('id', $gameIds[$i])->where('player_1', $id)->sum('player_1_critical_hits');
+            $critsAsP2InGame = PlayerStats::where('id', $gameIds[$i])->where('player_2', $id)->sum('player_2_critical_hits');
+
+            array_push($crits, $critsAsP1InGame + $critsAsP2InGame);
+        }
+
+        $averageCrits = $totalCrits / count($gameIds);
+        $averageCrits = round($averageCrits, 2);
+
+        $critsData = [
+            'total_critical_hits' => $totalCrits,
+            'crits' => $crits,
+            'average_critical_hits' => $averageCrits,
+        ];
+
+        return response()->json($critsData, 200);
     }
 
     public function getTotalUsedCards($id)
@@ -123,7 +220,32 @@ class PlayerStatsController extends Controller
         $usedCardsAsP2 = PlayerStats::where('player_2', $id)->sum('player_2_used_cards');
         $totalUsedCards = $usedCardsAsP1 + $usedCardsAsP2;
 
-        return response()->json($totalUsedCards, 200);
+        if ($totalUsedCards == 0) {
+            return response()->json(['message' => 'No used cards found']);
+        }
+
+        $usedCards = [];
+
+        $gameIds = PlayerStats::where('player_1', $id)->orWhere('player_2', $id)->get(['id'])->toArray();
+
+        for ($i = 0; $i < count($gameIds); $i++) {
+            $usedCardsAsP1InGame = PlayerStats::where('id', $gameIds[$i])->where('player_1', $id)->sum('player_1_used_cards');
+            $usedCardsAsP2InGame = PlayerStats::where('id', $gameIds[$i])->where('player_2', $id)->sum('player_2_used_cards');
+
+            array_push($usedCards, $usedCardsAsP1InGame + $usedCardsAsP2InGame);
+        }
+
+        $averageUsedCards = $totalUsedCards / count($gameIds);
+        $averageUsedCards = round($averageUsedCards, 2);
+
+        $usedCardsData = [
+            'total_used_cards' => $totalUsedCards,
+            'used_cards' => $usedCards,
+            'average_used_cards' => $averageUsedCards,
+        ];
+
+
+        return response()->json($usedCardsData, 200);
     }
 
 }
